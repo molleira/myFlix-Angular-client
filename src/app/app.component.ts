549092e29939380b9359e81d6ca477bd359a8ae1
Marc Oller <marc.om@icloud.com>
2021-03-23 19:23:05 +0100

    Implement Registration and Login forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
+// src/app/app.component.ts
 import { Component } from '@angular/core';
+
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
+
+  constructor(public dialog: MatDialog) { }
+  // opens the dialog when the signup button is clicked  
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      // assigns the dialog a width
+      width: '280px'
+    });
+  }
+
+  // opens the login dialog when the signup button is clicked
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      width: '280px'
+    });
+  }
+
 }
